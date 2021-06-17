3fea02c3c499110f337836ab4fcef05cf13262cf
Antoine <abugeia@gmail.com>
2021-06-17 15:52:14 +1100

    fixig app index page

--- app.py
+++ app.py
@@ -18,8 +18,31 @@ import joblib
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET', 'POST'])
+def HelloWorld():
+    # Retrieve the name from url parameter
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
 @app.route("/predict/", methods=['GET', 'POST'])
-def hello_world():
+def tags():
     request_type_str = request.method
     if request_type_str == 'GET':
         return render_template("tag-gen.html", href = 'Type a question')
